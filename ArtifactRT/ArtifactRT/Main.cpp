ce6933a992b698e7958bce620675d3ed1c5fdd61
Athos van Kralingen <athos.van.kralingen@hva.nl>
2021-03-21 22:39:25 +0100

    Re-add fuziness scene

--- ArtifactRT/ArtifactRT/Main.cpp
+++ ArtifactRT/ArtifactRT/Main.cpp
@@ -19,7 +19,7 @@ struct RenderTarget
 	constexpr static double AspectRatio = 16.0 / 9.0;
 	constexpr static int Width = 800;
 	constexpr static int Height = int(Width / AspectRatio);
-	constexpr static int SamplesPerPixel = 1000;
+	constexpr static int SamplesPerPixel = 100;
 };
 
 RGBColor SampleSkybox(const Ray& ray)
@@ -53,16 +53,13 @@ Scene CreateScene(Random& randomGenerator)
 	Scene scene;
 	std::unique_ptr<Material> material_ground = std::make_unique<Lambertian>(RGBColor(0.8, 0.8, 0.0), randomGenerator);
 	std::unique_ptr<Material> material_center_sphere = std::make_unique<Lambertian>(RGBColor(0.7, 0.3, 0.3), randomGenerator);
-	std::unique_ptr<Material> material_left_sphere = std::make_unique<Metalic>(RGBColor(0.8, 0.8, 0.8), 0.0f, randomGenerator);
-	std::unique_ptr<Material> material_right_sphere = std::make_unique<Metalic>(RGBColor(0.8, 0.6, 0.2), 0.0f, randomGenerator);
+	std::unique_ptr<Material> material_left_sphere = std::make_unique<Metalic>(RGBColor(0.8, 0.8, 0.8), 0.3f, randomGenerator);
+	std::unique_ptr<Material> material_right_sphere = std::make_unique<Metalic>(RGBColor(0.8, 0.6, 0.2), 1.0f, randomGenerator);
 	
 	scene.Add(std::make_unique<Sphere>(Point3( 0.0, -100.5, -1.0), 100.0, material_ground.get()));
-	scene.Add(std::make_unique<Sphere>(Point3( -0.8,    0.5, -1.0),   0.3, material_left_sphere.get()));
-	scene.Add(std::make_unique<Sphere>(Point3( -0.8,    0.0, -1.0),   0.3, material_left_sphere.get()));
-	scene.Add(std::make_unique<Sphere>(Point3( 0.8,    0.5, -1.0),   0.3, material_right_sphere.get()));
-	scene.Add(std::make_unique<Sphere>(Point3( 0.8,    0.0, -1.0),   0.3, material_right_sphere.get()));
-
-	scene.Add(std::make_unique<Sphere>(Point3(0,    0.2, -1.5),   0.5, material_center_sphere.get()));
+    scene.Add(std::make_unique<Sphere>(Point3( 0.0,    0.0, -1.0),   0.5, material_center_sphere.get()));
+    scene.Add(std::make_unique<Sphere>(Point3(-1.0,    0.0, -1.0),   0.5, material_left_sphere.get()));
+    scene.Add(std::make_unique<Sphere>(Point3( 1.0,    0.0, -1.0),   0.5, material_right_sphere.get()));
 	return scene;
 }
 
