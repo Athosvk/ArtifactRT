9f874af97994f73aa6ccaa26dbb4a75db5bcb554
Athos van Kralingen <Athosvk@users.noreply.github.com>
2025-07-05 08:20:37 +0200

    Add BVH toggle

--- ArtifactRT/ArtifactRT/Scene.cpp
+++ ArtifactRT/ArtifactRT/Scene.cpp
@@ -17,19 +17,30 @@ void Scene::AddMaterial(std::unique_ptr<Material> material)
 
 std::optional<IntersectionRecord> Scene::FindFirstIntersection(const Ray& ray, const SampleBounds& sampleBounds) const
 {
+	std::optional<IntersectionRecord> closest;
+
+#if defined(USE_BVH)
 	assert(m_BVH.has_value());
+	// Note: This currently doesn't correctly sample all primitives
 	auto intersection = m_BVH->FindFirstIntersection(ray, sampleBounds);
 	if (intersection) 
 	{
 		// TODO: Get a nearest T from the bvh so that we can compare to others,
 		// if needed
-		return intersection;
+		closest = intersection;
 	}
-	else
-	{
-		return {};
-	}
+#else
 
+	for (const Sphere& sphere : m_Spheres)
+	{
+		auto intersection = sphere.FindIntersection(ray, sampleBounds);
+		if (intersection && (!closest || intersection->T < closest->T))
+		{
+			closest = intersection;
+		}
+	}
+#endif
+	return closest;
 }
 
 void Scene::BuildBVH()
